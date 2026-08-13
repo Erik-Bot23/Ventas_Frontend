95b8366fcc755e0429b5b61466bf429c5797702d
Erik-Bot23 <erik@iracar-technologies.com>
2026-08-13 16:34:24 -0600

    Modulo de role y permisos listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,23 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { PermissionGuard } from './core/routes/guards/permission-guard';
+import { permission } from 'process';
 
 export const routes: Routes = [
     //Se definen las rutas de todas las paginas
     { path: '', loadComponent: () => import('./features/login/login').then(m=>m.Login) },
-    { path: 'cobro', loadComponent: () => import('./features/charge/cobro').then(m=>m.Cobro) },
+    { path: 'cobro', loadComponent: () => import('./features/charge/cobro').then(m=>m.Cobro), canActivate:[PermissionGuard], data:{permission: 'VER_COBRO'} },
     { path: 'perfil', loadComponent: () => import('./features/profile/perfil').then(m=>m.Perfil) },
-    { path: 'cliente', loadComponent: () => import('./features/clients/clientes').then(m=>m.Clientes) },
-    { path: 'compras', loadComponent: () => import('./features/shopping/compras').then(m=>m.Compras) },
-    { path: 'facturas', loadComponent: () => import('./features/invoices/facturas').then(m=>m.Facturas) },
-    { path: 'productos', loadComponent: () => import('./features/products/productos').then(m=>m.Productos) },
-    { path: 'reportes', loadComponent: () => import('./features/reports/reportes').then(m=>m.Reportes) },
-    { path: 'usuarios', loadComponent: () => import('./features/users/usuarios').then(m=>m.Usuarios) },
-    { path: 'ventas', loadComponent: () => import('./features/sales/ventas').then(m=>m.Ventas) },
-    { path: 'caja', loadComponent: () => import('./features/cash/caja').then(m=>m.Caja) },
-    { path: 'categorias', loadComponent: () => import('./features/categories/categorias').then(m=>m.Categorias) },
-    { path: 'roles', loadComponent: () => import('./features/roles/roles').then(m=>m.Roles) },
+    { path: 'cliente', loadComponent: () => import('./features/clients/clientes').then(m=>m.Clientes), canActivate:[PermissionGuard], data:{permission: 'VER_CLIENTES'} },
+    { path: 'compras', loadComponent: () => import('./features/shopping/compras').then(m=>m.Compras), canActivate:[PermissionGuard], data:{permission: 'VER_COMPRAS'} },
+    { path: 'facturas', loadComponent: () => import('./features/invoices/facturas').then(m=>m.Facturas), canActivate:[PermissionGuard], data:{permission: 'VER_FACTURAS'} },
+    { path: 'productos', loadComponent: () => import('./features/products/productos').then(m=>m.Productos), canActivate:[PermissionGuard], data:{permission: 'VER_PRODUCTOS'} },
+    { path: 'reportes', loadComponent: () => import('./features/reports/reportes').then(m=>m.Reportes), canActivate:[PermissionGuard], data:{permission: 'VER_REPORTES'} },
+    { path: 'usuarios', loadComponent: () => import('./features/users/usuarios').then(m=>m.Usuarios), canActivate:[PermissionGuard], data:{permission: 'VER_USUARIOS'} },
+    { path: 'ventas', loadComponent: () => import('./features/sales/ventas').then(m=>m.Ventas), canActivate:[PermissionGuard], data:{permission: 'VER_VENTAS'} },
+    { path: 'caja', loadComponent: () => import('./features/cash/caja').then(m=>m.Caja), canActivate:[PermissionGuard], data:{permission: 'VER_CAJA'} },
+    { path: 'categorias', loadComponent: () => import('./features/categories/categorias').then(m=>m.Categorias), canActivate:[PermissionGuard], data:{permission: 'VER_CATEGORIAS'} },
+    { path: 'roles', loadComponent: () => import('./features/roles/roles').then(m=>m.Roles), canActivate:[PermissionGuard], data:{permission: 'VER_ROLES'} },
     { path: 'forgot-password', loadComponent: () => import('./features/login/forgot-password/forgot-password').then(m=>m.ForgotPassword) },
     { path: 'reset-password', loadComponent: () => import('./features/login/reset-password/reset-password').then(m=>m.ResetPassword) },
     { path: '**', redirectTo: '' }
--- src/app/features/sidebar/sidebar.ts
+++ src/app/features/sidebar/sidebar.ts
@@ -2,10 +2,12 @@ import { Component, Output, EventEmitter, ChangeDetectorRef } from '@angular/cor
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/service/auth-service/auth-service'; 
 import { permission } from 'process';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
@@ -13,7 +15,7 @@ export class Sidebar {
   constructor(
     private router: Router,
     private cd: ChangeDetectorRef,
-    private auth: AuthService){}
+    public auth: AuthService){}
 
   menuOpen = false;
   @Output() toggle = new EventEmitter<boolean>();
@@ -37,9 +39,29 @@ export class Sidebar {
       permission: 'VER_PRODUCTOS'
     },
     {
+      label: 'Categorías',
+      route: '/categorias',
+      permission: 'VER_CATEGORIAS'
+    },
+    {
+      label: 'Caja',
+      route: '/caja',
+      permission: 'VER_CAJA'
+    },
+    {
       label: 'Usuarios',
       route: '/usuarios',
       permission: 'VER_USUARIOS'
+    },
+    {
+      label: 'Roles',
+      route: '/roles',
+      permission: 'VER_ROLES'
+    },
+    {
+      label: 'Clientes',
+      route: '/clientes',
+      permission: 'VER_CLIENTES'
     },
     {
       label: 'Ventas',
@@ -47,14 +69,23 @@ export class Sidebar {
       permission: 'VER_VENTAS'
     },
     {
-      label: 'Roles',
-      route: '/roles',
-      permission: 'VER_ROLES'
+      label: 'Compras',
+      route: '/compras',
+      permission: 'VER_COMPRAS'
+    },
+    {
+      label: 'Reportes',
+      route: '/reportes',
+      permission: 'VER_REPORTES'
+    },
+    {
+      label: 'Facturas',
+      route: '/facturas',
+      permission: 'VER_FACTURAS'
     }
   ]
 
-
-  //
+  //Navegar entre rutas
   go(route: string){
     this.router.navigate([route]);
   }
@@ -64,47 +95,7 @@ export class Sidebar {
     this.router.navigate(['/perfil']);
   }
 
-  ventas(){
-    this.router.navigate(['/ventas']);
-  }
-
   inicio(){
     this.router.navigate(['/cobro']);
-  }
-
-  caja(){
-    this.router.navigate(['/caja']);
-  }
-
-  clientes(){
-    this.router.navigate(['/cliente']);
-  }
-
-  compras(){
-    this.router.navigate(['/compras']);
-  }
-
-  facturas(){
-    this.router.navigate(['/facturas']);
-  }
-
-  productos(){
-    this.router.navigate(['/productos']);
-  }
-
-  categorias(){
-    this.router.navigate(['/categorias'])
-  }
-
-  reportes(){
-    this.router.navigate(['/reportes']);
-  }
-
-  usuarios(){
-    this.router.navigate(['/usuarios']);
-  }
-
-  roles(){
-    this.router.navigate(['/roles']);
   }
 }
